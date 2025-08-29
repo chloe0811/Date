a6a31eb75270206a8b885adf1af6579f556577ca
chloe0811 <167526055+chloe0811@users.noreply.github.com>
2025-08-29 12:20:30 +1000

    update

---
+++ script.js
@@ -0,0 +1,11 @@
+function handleYesClick() {
+  window.location.href = "yes_page.html";
+}
+
+function handleNoClick() {
+  alert("Okay 👀 I’ll give you a tiny hint… but not too much 😉");
+}
+
+function handleMaybeClick() {
+  alert("Aww, rain check it is… but don’t forget, you owe me DOUBLE dessert next time 🍨🍰");
+}
